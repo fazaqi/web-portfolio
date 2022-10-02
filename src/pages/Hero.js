c9e6d30465ed557caf623e61fb9a40d5e22d064c
ezasyq <eza.yokesen@gmail.com>
2022-10-02 16:44:23 +0700

    first initialization

---
+++ src/pages/Hero.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <>
+      <div className="container">
+        <h5>Hi, my name is</h5>
+        <h1>Fariz Eza Syauqi.</h1>
+        <p>
+          I’m a software engineer specializing in building (and occasionally
+          designing) exceptional digital experiences. Currently, I’m focused on
+          building accessible, human-centered products at Upstatement.
+        </p>
+        <button>Check</button>
+      </div>
+    </>
+  );
+}
+
+export default Hero;
