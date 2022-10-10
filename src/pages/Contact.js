ac321195f94d2fabbb5c375b412a312aab6f5576
fazaqi <syauqieza@gmail.com>
2022-10-10 17:45:25 +0700

    added wordings

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,16 +7,17 @@ function Contact() {
         <div className="container text-center">
           <h2>Contact Me</h2>
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste fuga
-            at rerum obcaecati tempora, delectus ab debitis molestiae est
-            corporis, impedit nemo, in adipisci quia.
+            I'm currently seeking a new opportunity. My inbox is always open, so
+            feel free to contact me if you are interested in knowing more about
+            me or if you need me to build a website / web app for you. I will do
+            my best to help.
           </p>
           <a
             href="mailto:syauqieza@gmail.com"
             target="_blank"
             rel="noreferrer noopener"
           >
-            <button>Say Hello</button>
+            <button>Say Hi!</button>
           </a>
         </div>
       </section>
